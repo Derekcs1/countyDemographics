eae238f8d1f04f1c06cdb200b2c564a0ea8561b7
michaelmoschitto <mikemoschitto@gmail.com>
2017-11-07 09:54:35 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -6,11 +6,12 @@ app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  
 
 @app.route("/")
 def render_main():
+    <!-- 
     with open('static/county_demographics.json') as demographicsdata
         counties = json.load(demographicsdata)
     if 'State' in request.args:
         sel_state = request.args["State"]
-        return render_template('home.html', response_options = get_state_options(counties), response_population = get_2014_population(counties, sel_state), response_state = sel_state)
+        return render_template('home.html', response_options = get_state_options(counties), fact = func(), response_state = sel_state)
     return render_template('home.html', response_options = get_state_options(counties))
 
 
@@ -23,12 +24,10 @@ def get_state_options(counties):
             options += Markup("<option value=\"" + c["State"] + "\">" + c["State"] + "</option>")
     return options
 
-def get_2014_population(counties, selected_state):
-    total = 0
-    for c in counties:
-        if c["State"] == selected_state:
-            total_population += c["Population"]["2014 Population"]
-    return str(total_population)
+def func():
+    
+    return str(1) 
+-->
 
 
 if __name__=="__home__":

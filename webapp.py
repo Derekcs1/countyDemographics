5eed4b62b6865be344705a01ec45a67bba5bb7c4
michaelmoschitto <mikemoschitto@gmail.com>
2017-11-09 09:48:17 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,7 +10,7 @@ def render_main():
         counties = json.load(demographics_data)
     if 'State' in request.args:
         selected_state = request.args["State"]
-        return render_template('home.html', response_options = get_state_options(counties), response_population = percentUnder18(counties, selected_state), response_state = selected_state)
+        return render_template('home.html', response_options = get_state_options(counties), percentUnder18 = percentUnder18(counties, selected_state), response_state = selected_state)
     return render_template('home.html', response_options = get_state_options(counties))
 
 def get_state_options(counties):
